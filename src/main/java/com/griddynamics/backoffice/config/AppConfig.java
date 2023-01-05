9a071eed33d10f83d5fcb87964c29ab4cad29d94
damski-ugodnik <ponomarenkonikita03@gmail.com>
2023-01-05 14:30:59 +0200

    pattern change

--- src/main/java/com/griddynamics/backoffice/config/AppConfig.java
+++ src/main/java/com/griddynamics/backoffice/config/AppConfig.java
@@ -1,26 +1,16 @@
 package com.griddynamics.backoffice.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class AppConfig {
     @Bean
     public ObjectMapper objectMapper() {
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-        LocalDateTimeDeserializer localDateTimeDeserializer = new
-                LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
-        javaTimeModule.addDeserializer(LocalDateTime.class, localDateTimeDeserializer);
-        return Jackson2ObjectMapperBuilder.json()
-                .modules(javaTimeModule)
-                .build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 }
